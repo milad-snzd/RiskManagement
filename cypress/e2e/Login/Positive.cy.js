a5fbbe1a312142323c0c1b5b4bb8d0d601f33513
QA <milad.snzd@gmail.com>
2024-12-29 13:23:22 +0330

    Add Cypress In Project Riskmanegement

---
+++ cypress/e2e/Login/Positive.cy.js
@@ -0,0 +1,6 @@
+/// <reference types="cypress"/>
+describe('Login Input' , ()=>{
+    it.only('Correct Value' , ()=>{
+        cy.visit('https://riskmanagement-stage.otcsaba.ir/login')
+    })
+})
---
+++ cypress/e2e/Login/negative.cy.js
@@ -0,0 +1,6 @@
+/// <reference types="cypress"/>
+// describe('Login Input' , ()=>{
+//     it('Incorrect Value' , ()=>{
+        
+//     })
+// })
