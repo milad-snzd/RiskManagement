4c6d463e17c9ee2d093638f70ee5f2eb646ce05c
QA <milad.snzd@gmail.com>
2024-12-29 12:00:10 +0330

    Delete Additional Folder

--- Detail Page/negative.cy.js
+++
@@ -1,6 +0,0 @@
-/// <reference types="cypress"/>
-describe('input' , ()=>{
-    it('current value' , ()=>{
-        
-    })
-})
--- Home Page/positive.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress"/>
-describe('input' , ()=>{
-    it('current value' , ()=>{
-        
-    })
-    it('incurrect value' , ()=>{
-        cy.visit=" https://riskmanagement-stage.otcsaba.ir/"
-    })
-})
