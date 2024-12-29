0afefed2b0f2b0c32fb63b3a3d07993f866e8426
QA <milad.snzd@gmail.com>
2024-12-29 15:25:22 +0330

    Add Empty Folder

---
+++ cypress/e2e/Login/Empty.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress"/>
+describe(
+    "Login Input",
+    {
+      viewportHeight: 810,
+      viewportWidth: 1440,
+    },
+    () => {
+      it.only("Empty Value", () => {
+        cy.visit("https://riskmanagement-stage.otcsaba.ir/login");
+      });
+    }
+  );
--- cypress/e2e/Login/Positive.cy.js
+++ cypress/e2e/Login/Positive.cy.js
@@ -8,6 +8,9 @@ describe(
   () => {
     it.only("Correct Value", () => {
       cy.visit("https://riskmanagement-stage.otcsaba.ir/login");
+      cy.get('[data-test="64dee3ca-87c2-4c17-95ae-01afc8d4e01e"]').type('administrator')
+      cy.get('[data-test="98e1eb98-87b2-4112-a3c9-6a23b29c3fea"]').type('Admin@123')
+      
     });
   }
 );
