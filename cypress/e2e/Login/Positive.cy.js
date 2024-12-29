617c2f52605ad405c2a5c29bbe079a0c3d9c5b26
QA <milad.snzd@gmail.com>
2024-12-29 13:40:22 +0330

    Add Standard Veiwport In Project

--- cypress/e2e/Login/Positive.cy.js
+++ cypress/e2e/Login/Positive.cy.js
@@ -1,6 +1,13 @@
 /// <reference types="cypress"/>
-describe('Login Input' , ()=>{
-    it.only('Correct Value' , ()=>{
-        cy.visit('https://riskmanagement-stage.otcsaba.ir/login')
-    })
-})
+describe(
+  "Login Input",
+  {
+    viewportHeight: 810,
+    viewportWidth: 1440,
+  },
+  () => {
+    it.only("Correct Value", () => {
+      cy.visit("https://riskmanagement-stage.otcsaba.ir/login");
+    });
+  }
+);
--- cypress/e2e/Login/negative.cy.js
+++ cypress/e2e/Login/negative.cy.js
@@ -1,6 +1,13 @@
 /// <reference types="cypress"/>
-// describe('Login Input' , ()=>{
-//     it('Incorrect Value' , ()=>{
-        
-//     })
-// })
+describe(
+    "Login Input",
+    {
+      viewportHeight: 810,
+      viewportWidth: 1440,
+    },
+    () => {
+      it.only("InCorrect Value", () => {
+        cy.visit("https://riskmanagement-stage.otcsaba.ir/login");
+      });
+    }
+  );
