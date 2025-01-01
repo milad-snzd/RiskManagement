330ee55bbf57e3556b95dcddb7f9548eff351d34
QA <milad.snzd@gmail.com>
2025-01-01 10:28:04 +0330

    add test unit login with the correct username and password

--- cypress/e2e/Unit Test Login/Positive.cy.js
+++ cypress/e2e/Unit Test Login/Positive.cy.js
@@ -1,13 +1,33 @@
 /// <reference types="cypress"/>
 describe(
-  "Login Input",
+  "Login Positive Test",
   {
     viewportHeight: 810,
     viewportWidth: 1440,
   },
   () => {
-    it.only("Correct Value", () => {
-      cy.visit("https://riskmanagement-stage.otcsaba.ir/login");
+    it.only("Log in with the correct username and password", () => {
+      cy.visit("https://riskmanagement-stage.otcsaba.ir");
+      cy.get(".text-textWhite > .flex > .text-lg").contains(
+        "سامانه مدیریت ریسک"
+      );
+      cy.get(".pb-7").contains("ورود کاربر");
+      cy.get('[data-test="64dee3ca-87c2-4c17-95ae-01afc8d4e01e"]')
+        .click()
+        .wait(1000)
+        .type("administrator");
+      cy.get('[data-test="98e1eb98-87b2-4112-a3c9-6a23b29c3fea"]')
+        .click()
+        .wait(1000)
+        .type("Admin@123");
+      cy.get('[data-test="559b63a0-af19-4aa1-9643-e333fafefe3d"]')
+        .contains("ورود")
+        .click();
+      cy.request("https://riskmanagement-stage.otcsaba.ir/login").then(
+        ($response) => {
+          expect($response.status).to.eq(200);
+        }
+      );
     });
   }
 );
