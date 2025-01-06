30e6e040b50b939474ba3979e41b5c1bb002631b
QA <milad.snzd@gmail.com>
2025-01-06 16:14:53 +0330

    Add Negative test Case and Folder

--- cypress/e2e/Unit Test Login/negative.cy.js
+++ cypress/e2e/Unit Test Login/negative.cy.js
@@ -72,10 +72,6 @@ describe(
         "سامانه مدیریت ریسک"
       );
       cy.get(".pb-7").contains("ورود کاربر");
-      cy.get('[data-test="64dee3ca-87c2-4c17-95ae-01afc8d4e01e"]')
-        .click()
-        .wait(1000)
-        .type("");
       cy.get('[data-test="98e1eb98-87b2-4112-a3c9-6a23b29c3fea"]')
         .click()
         .wait(1000)
@@ -92,6 +88,129 @@ describe(
         }
       );
       cy.get('[data-test="64dee3ca-87c2-4c17-95ae-01afc8d4e01e-error"]').should(
+        "contain",
+        "نام کاربری یا رمز عبور صحیح نیست"
+      );
+    });
+    it("Entering an empty password field", () => {
+      cy.visit("https://riskmanagement-stage.otcsaba.ir/login");
+      cy.get(".text-textWhite > .flex > .text-lg").contains(
+        "سامانه مدیریت ریسک"
+      );
+      cy.get(".pb-7").contains("ورود کاربر");
+      cy.get('[data-test="64dee3ca-87c2-4c17-95ae-01afc8d4e01e"]')
+        .click()
+        .type("Fs0");
+      cy.get(
+        '[data-test="98e1eb98-87b2-4112-a3c9-6a23b29c3fea-show-pass-icon"]'
+      ).click();
+      cy.get('[data-test="559b63a0-af19-4aa1-9643-e333fafefe3d"]')
+        .contains("ورود")
+        .click();
+      cy.request("https://riskmanagement-stage.otcsaba.ir/login").then(
+        ($response) => {
+          expect($response.status).to.eq(200);
+        }
+      );
+      cy.get('[data-test="98e1eb98-87b2-4112-a3c9-6a23b29c3fea-error"]').should(
+        "contain",
+        "نام کاربری یا رمز عبور صحیح نیست"
+      );
+    });
+    it("Entering a username that does not exist in the system", () => {
+      cy.visit("https://riskmanagement-stage.otcsaba.ir/login");
+      cy.get(".text-textWhite > .flex > .text-lg").contains(
+        "سامانه مدیریت ریسک"
+      );
+      cy.get(".pb-7").contains("ورود کاربر");
+      cy.get('[data-test="64dee3ca-87c2-4c17-95ae-01afc8d4e01e"]')
+        .click()
+        .type("AAAAAAAAAAAAAAA");
+      cy.get('[data-test="98e1eb98-87b2-4112-a3c9-6a23b29c3fea"]').type(
+        "Ms0480371261@"
+      );
+      cy.get(
+        '[data-test="98e1eb98-87b2-4112-a3c9-6a23b29c3fea-show-pass-icon"]'
+      ).click();
+      cy.get('[data-test="559b63a0-af19-4aa1-9643-e333fafefe3d"]')
+        .contains("ورود")
+        .click();
+      cy.request("https://riskmanagement-stage.otcsaba.ir/login").then(
+        ($response) => {
+          expect($response.status).to.eq(200);
+        }
+      );
+      cy.get('[data-test="64dee3ca-87c2-4c17-95ae-01afc8d4e01e-error"]').should(
+        "contain",
+        "نام کاربری یا رمز عبور صحیح نیست"
+      );
+      cy.get('[data-test="98e1eb98-87b2-4112-a3c9-6a23b29c3fea-error"]').should(
+        "contain",
+        "نام کاربری یا رمز عبور صحیح نیست"
+      );
+    });
+    it("Testing login with excessive length usernames and passwords", () => {
+      cy.visit("https://riskmanagement-stage.otcsaba.ir/login");
+      cy.get(".text-textWhite > .flex > .text-lg").contains(
+        "سامانه مدیریت ریسک"
+      );
+      cy.get(".pb-7").contains("ورود کاربر");
+      cy.get('[data-test="64dee3ca-87c2-4c17-95ae-01afc8d4e01e"]')
+        .click()
+        .type(
+          "sdkjfhlkjhafkjhsldkjhflkjahkdsjhflkjhakjhdslkjhfakjhsdkfhlakjhsdfkhkjdhflahjfdkjshflkjhasdkflhakdjshlkhjsakjfhkdjhfakjhdfkjlhakdjshkjhdfkjhdkfjkdjhkdjhfkjhdkfjkdjfkf"
+        );
+      cy.get('[data-test="98e1eb98-87b2-4112-a3c9-6a23b29c3fea"]').type(
+        "Ms0480371261@ajhsgdfkjhgkdjshfkjhgdskjhfkjhsdgkfjhgfkjhgsdkjfgkjsdhgfkjhgsdkjfkjshgfkjhgsdkfgkjsdgfkjbjbcgfksdgfxjhbxkjbxkjfskjfgkjsdfhgksdjbfxkjxbxfkjbkfjhsgdkjfgxbsjhk"
+      );
+      cy.get(
+        '[data-test="98e1eb98-87b2-4112-a3c9-6a23b29c3fea-show-pass-icon"]'
+      ).click();
+      cy.get('[data-test="559b63a0-af19-4aa1-9643-e333fafefe3d"]')
+        .contains("ورود")
+        .click();
+      cy.request("https://riskmanagement-stage.otcsaba.ir/login").then(
+        ($response) => {
+          expect($response.status).to.eq(200);
+        }
+      );
+      cy.get('[data-test="64dee3ca-87c2-4c17-95ae-01afc8d4e01e-error"]').should(
+        "contain",
+        "نام کاربری یا رمز عبور صحیح نیست"
+      );
+      cy.get('[data-test="98e1eb98-87b2-4112-a3c9-6a23b29c3fea-error"]').should(
+        "contain",
+        "نام کاربری یا رمز عبور صحیح نیست"
+      );
+    });
+    it("Testing login with incorrect case (uppercase/lowercase) in the username", () => {
+      cy.visit("https://riskmanagement-stage.otcsaba.ir/login");
+      cy.get(".text-textWhite > .flex > .text-lg").contains(
+        "سامانه مدیریت ریسک"
+      );
+      cy.get(".pb-7").contains("ورود کاربر");
+      cy.get('[data-test="64dee3ca-87c2-4c17-95ae-01afc8d4e01e"]')
+        .click()
+        .type("AAAAsdmnsd,smnd,m65435435435");
+      cy.get('[data-test="98e1eb98-87b2-4112-a3c9-6a23b29c3fea"]').type(
+        "Ms0480371261@"
+      );
+      cy.get(
+        '[data-test="98e1eb98-87b2-4112-a3c9-6a23b29c3fea-show-pass-icon"]'
+      ).click();
+      cy.get('[data-test="559b63a0-af19-4aa1-9643-e333fafefe3d"]')
+        .contains("ورود")
+        .click();
+      cy.request("https://riskmanagement-stage.otcsaba.ir/login").then(
+        ($response) => {
+          expect($response.status).to.eq(200);
+        }
+      );
+      cy.get('[data-test="64dee3ca-87c2-4c17-95ae-01afc8d4e01e-error"]').should(
+        "contain",
+        "نام کاربری یا رمز عبور صحیح نیست"
+      );
+      cy.get('[data-test="98e1eb98-87b2-4112-a3c9-6a23b29c3fea-error"]').should(
         "contain",
         "نام کاربری یا رمز عبور صحیح نیست"
       );
